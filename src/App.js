6e34978b770d61d3bf2022d284fccd237f0f7475
Ayon Chakroborty <gv5072@Wayne.edu>
2022-08-07 14:47:42 -0400

    Changed style and added Banner component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/New Expense/NewExpense';
+import Banner from './components/UI/Banner';
 
 const DUMMY_EXPENSES = [
   { id: 'e1', title: "Car Title", date: new Date(2021, 2, 28), amount: 284.64 },
@@ -22,6 +23,7 @@ function App() {
   
   return (
     <div>
+      <Banner />
       <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses item={expenses} /> 
     </div>
---
+++ src/components/UI/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css';
+
+const Banner = () => {
+  return (
+    <div class="banner__box_area">
+        <div class="banner__text">
+            <h1>EXPENSE TRACKER</h1>
+        </div>
+      </div>
+  );
+};
+
+export default Banner;
